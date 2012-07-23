8af194ba380b8963f31def827e302a7f3db28d77
dvir volk <dvir@doit9.com>
2012-07-24 00:58:56 +0300

    incrmeneting version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='kickass_redis',
-    version='0.1.3-1',
+    version='0.1.4',
     package_dir={'kickass_redis': 'src'},
     packages=['kickass_redis', 'kickass_redis.patterns', 'kickass_redis.patterns.object_store'],
     requires=['redis'],
